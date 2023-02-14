f1ea4075441695a327095ba2586b6da9d70b331d
whiskascats <whiskascats@users.noreply.github.com>
2023-02-14 08:15:54 +0000

    deploy: 3fc5a701ecae6d2951c8385d62f2b6a66e217d9c

--- src/App.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-import test from './views/test.vue'
-</script>
-
-<template>
-  <test />
-</template>
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
-
-import '@/assets/style.css'
-
-createApp(App).mount('#app')
